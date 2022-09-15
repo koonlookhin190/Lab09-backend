14d9eec14e524dde0d9f754b03e290ced1b89ac8
koonlookhin <krit_tipnuan@cmu.ac.th>
2022-09-15 21:20:33 +0700

    5.8

--- src/main/java/se331/rest/dao/EventDao.java
+++ src/main/java/se331/rest/dao/EventDao.java
@@ -1,12 +1,12 @@
 package se331.rest.dao;
 
 import se331.rest.entity.Event;
-
+import org.springframework.data.domain.Page;
 import java.util.List;
 
 public interface EventDao {
     Integer getEventSize();
-    List<Event> getEvents(Integer pageSize, Integer page);
+    Page<Event> getEvents(Integer pageSize, Integer page);
     Event getEvent(Long id);
 }
 
